3099a813d542b38b70abca49d7f644f55e935602
shishkin015 <shishkin015@gmail.com>
2024-03-03 21:45:16 +0300

    permission

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import UserViewSet
@@ -10,5 +11,6 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
               ] + router.urls
